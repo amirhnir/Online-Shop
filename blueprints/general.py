fd214dc5b76b29221176a079c0009eeef3c907cb
AmirHossein Naei <info@amirhn.ir>
2023-05-13 15:29:27 +0430

    route (about) added

--- blueprints/general.py
+++ blueprints/general.py
@@ -2,6 +2,12 @@ from flask import Blueprint
 
 app = Blueprint("general", __name__)
 
+
 @app.route('/')
-def hello_world():
+def main():
     return 'this is main page'
+
+
+@app.route('/about')
+def about():
+    return 'about us'
