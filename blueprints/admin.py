769ffeeb13ff09b3abb3dfccc26f553b2ea4b08c
AmirHossein Naei <info@amirhn.ir>
2023-05-27 12:51:41 +0330

    admin edit product added

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session, redirect, abort
+from flask import Blueprint, render_template, request, session, redirect, abort, url_for
 import config
 from models.product import Product
 from extentions import db
@@ -54,3 +54,28 @@ def products():
         db.session.commit()
 
         return "done"
+
+
+@app.route('/admin/dashboard/edit-product/<id>', methods=["GET", "POST"])
+def edit_product(id):
+    product = Product.query.filter(Product.id == id).first_or_404()
+
+    if request.method == "GET":
+        return render_template("admin/edit-product.html", product=product)
+    else:
+        name = request.form.get('name', None)
+        description = request.form.get('description', None)
+        price = request.form.get('price', None)
+        active = request.form.get('active', None)
+
+        product.name = name
+        product.description = description
+        product.price = price
+        if active == None:
+            product.active = 0
+        else:
+            product.active = 1
+
+        db.session.commit()
+
+        return redirect(url_for("admin.edit_product", id=id))
