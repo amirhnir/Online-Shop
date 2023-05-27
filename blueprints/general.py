fd824e26a1240f3ac2615f5b38563288450b738e
AmirHossein Naei <info@amirhn.ir>
2023-05-27 13:20:09 +0330

    main and about added

--- blueprints/general.py
+++ blueprints/general.py
@@ -1,13 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
+from models.product import Product
 
 app = Blueprint("general", __name__)
 
 
 @app.route('/')
 def main():
-    return 'this is main page'
+    products = Product.query.all()
+    return render_template('main.html', products=products)
 
 
 @app.route('/about')
 def about():
-    return 'about us'
+    return render_template('about.html')
