20d853cd4820f7e9c74d013e95c8e522198acba3
AmirHossein Naei <info@amirhn.ir>
2023-05-27 16:31:05 +0330

    product page added

--- blueprints/general.py
+++ blueprints/general.py
@@ -7,8 +7,15 @@ app = Blueprint("general", __name__)
 
 @app.route('/')
 def main():
-    products = Product.query.all()
+    products = Product.query.filter(Product.active == 1).all()
     return render_template('main.html', products=products)
+
+
+@app.route('/product/<int:id>/<name>')
+def product(id, name):
+    product = Product.query.filter(Product.id == id).filter(Product.name == name).filter(
+        Product.active == 1).first_or_404()
+    return render_template('product.html', product=product)
 
 
 @app.route('/about')
