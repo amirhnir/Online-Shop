847a8609447f3fb67e233cb9cdd8297d43ecd169
AmirHossein Naei <info@amirhn.ir>
2023-05-31 18:06:34 +0330

    payment gateway (shepa)  added

--- blueprints/user.py
+++ blueprints/user.py
@@ -2,10 +2,13 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, login_required, current_user
 from passlib.hash import sha256_crypt
 from extentions import db
+from models.Payment import Payment
 from models.cart import Cart
 from models.cart_item import CartItem
 from models.product import Product
 from models.user import User
+import requests
+import  config
 
 app = Blueprint("user", __name__)
 
@@ -50,7 +53,6 @@ def login():
         return 'done'
 
 
-
 @app.route('/add-to-cart', methods=['GET'])
 @login_required
 def add_to_cart():
@@ -92,12 +94,66 @@ def remove_from_cart():
     return redirect(url_for('user.cart'))
 
 
-
 @app.route('/cart', methods=['GET'])
 @login_required
 def cart():
     cart = current_user.carts.filter(Cart.status == "pending").first()
     return render_template('user/cart.html', cart=cart)
+
+
+@app.route('/payment', methods=['GET'])
+@login_required
+def payment():
+    cart = current_user.carts.filter(Cart.status == 'pending').first()
+
+    r = requests.post(config.PAYMENT_FIRST_REQUEST_URL,
+                      data={
+                          'api':config.PAYMENT_MERCHANT,
+                          'amount': cart.total_price(),
+                          'callback':config.PAYMENT_CALLBACK
+                      })
+    token = r.json()['result']['token']
+    url = r.json()['result']['url']
+
+    pay = Payment(price=cart.total_price(), token=token)
+    pay.cart = cart
+    db.session.add(pay)
+    db.session.commit()
+
+    return redirect(url)
+
+
+@app.route('/verify', methods=['GET'])
+@login_required
+def verify():
+    token = request.args.get('token')
+    pay = Payment.query.filter(Payment.token == token).first_or_404()
+    r = requests.post(config.PAYMENT_VERIFY_REQUEST_URL,
+                      data={
+                          'api': 'sandbox',
+                          'amount': pay.price,
+                          'token': token
+                      })
+
+    pay_status = bool(r.json()['success'])
+    if pay_status:
+        transaction_id = r.json()['result']['transaction_id']
+        refid = r.json()['result']['refid']
+        card_pan = r.json()['result']['card_pan']
+
+        pay.card_pan= card_pan
+        pay.transaction_id = transaction_id
+        pay.refid = refid
+        pay.status = 'success'
+        pay.cart.status = 'paid'
+        flash("پرداخت موفق آمیز بود")
+    else:
+        flash("پرداخت با خطا مواجه شد")
+        pay.status = 'failed'
+
+    db.session.commit()
+
+    return redirect(url_for('user.dashboard'))
 
 
 @app.route('/user/dashboard', methods=['GET'])
--- config.py
+++ config.py
@@ -3,5 +3,9 @@ SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
 ADMIN_USERNAME = "admin"
 ADMIN_PASSWORD = "1234"
 
-
 SECRET_KEY = "sdaksd$$%%asd@@jsdljka435345hsdohssasjld"
+
+PAYMENT_MERCHANT = "sandbox"
+PAYMENT_CALLBACK = "http://localhost:5000/verify"
+PAYMENT_FIRST_REQUEST_URL = 'https://sandbox.shepa.com/api/v1/token'
+PAYMENT_VERIFY_REQUEST_URL = 'https://sandbox.shepa.com/api/v1/verify'
