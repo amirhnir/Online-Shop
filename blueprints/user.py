2645ef11d2f567329c24c29b8a33889b7b81d47c
AmirHossein Naei <info@amirhn.ir>
2023-06-04 11:47:59 +0330

    better design

--- blueprints/user.py
+++ blueprints/user.py
@@ -16,6 +16,8 @@ app = Blueprint("user", __name__)
 @app.route('/user/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
+        if current_user.is_authenticated :
+            return redirect(url_for('user.dashboard'))
         return render_template('user/login.html')
     else:
         register = request.form.get('register', None)
